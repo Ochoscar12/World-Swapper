e671fb682eee1d7cdf28becdf9ace3350d6e15d9
Ochoscar <ochoscar_ochoa_chavez@outlook.com>
2025-06-06 21:00:18 -0600

    ActualizacionUltimate
    Proyecto terminado (espero)

---
+++ Assets/Menu/UI Terminado/scripts/Menu/MenuSystem.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+public class MenuSystem : MonoBehaviour
+{
+   public void Jugar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+   public void Salir()
+    {
+        Debug.Log("Saliendo del juego....");
+        Application.Quit();
+    }
+}
